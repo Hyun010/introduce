05f4ffc5e3916b372d7c536e2a4a6ce926a45e7c
Park SungHyun <sunghyun0620@gmail.com>
2024-02-14 17:01:29 +0900

    초기설정

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/<name>/')
+def profile(name):
+    # name 을 통한 데이터 조회
+    context = {
+
+    }
+    return render_template('profile.html', data=context)
+
+if __name__ == '__main__':
+    app.run(debug=True)
