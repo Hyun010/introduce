d3582df14694a1e069fede9e08e7e8cd46a3a064
Park SungHyun <sunghyun0620@gmail.com>
2024-02-14 19:05:08 +0900

    성현 소개페이지 이동연결

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/박성현/')
+def myPage():
+    # name 을 통한 데이터 조회
+    context = {
+
+    }
+    return render_template('myPage.html', data=context)
+
 @app.route('/<name>/')
 def profile(name):
     # name 을 통한 데이터 조회
@@ -13,6 +21,7 @@ def profile(name):
 
     }
     return render_template('profile.html', data=context)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
