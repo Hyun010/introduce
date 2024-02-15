df62a00424154f6476c4e5c1ee57d408e71adf89
jejuHyeop <sh.han@yoil.co.kr>
2024-02-15 15:37:37 +0900

    sh 15:40

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
 from flask import Flask, render_template
-#1
+from database import db, Student
+import os
+
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+db.init_app(app)
+
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/박성현/')
-def myPage():
-    # name 을 통한 데이터 조회
+def home():
+    s = Student.query.all()
     context = {
-
+        "stus" : s
     }
-    return render_template('myPage.html', data=context)
+    return render_template('index.html', data=context)
 
 @app.route('/<name>/')
 def profile(name):
     # name 을 통한 데이터 조회
+    s1 = Student.query.filter_by(name=name).first()
     context = {
-
+        "stu" : s1
     }
     return render_template('profile.html', data=context)
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+with app.app_context():
+    db.create_all()
